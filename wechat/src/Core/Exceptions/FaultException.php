ae7d2341b16b9714c59b53ca41d6c1a8e98dc257
huangfeilong <huangfeilong83@126.com>
2016-08-24 14:28:17 +0800

    更新核心包

---
+++ wechat/src/Core/Exceptions/FaultException.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the huangro/laravel-payment.
+ *
+ * (c) robin <robin@v5club.cn>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * FaultException.php.
+ *
+ * @author    robin <robin@v5club.cn>
+ * @copyright 2015 robin <robin@v5club.cn>
+ *
+ * @link      https://github.com/huangro
+ */
+namespace TYWeChat\Core\Exceptions;
+
+use TYWeChat\Core\Exception;
+
+/**
+ * Class FaultException.
+ */
+class FaultException extends Exception
+{
+}
